4124eab15ca6c58bde902ea2fa3b668d46835830
Andrew Christensen <andrew.christensen014@gmail.com>
2020-02-22 18:46:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
-
-console.log('Hello, ' + specialGuest + '!');
